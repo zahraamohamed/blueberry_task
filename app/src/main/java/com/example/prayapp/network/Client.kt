d92be0287518b0b29cec4ef6b0047cd33efd6124
mohammedmaher7 <heymega7@gmail.com>
2021-09-25 22:05:25 +0300

    feat:add client object and repository class

--- app/src/main/java/com/example/prayapp/network/Client.kt
+++ app/src/main/java/com/example/prayapp/network/Client.kt
@@ -1,10 +1,37 @@
 package com.example.prayapp.network
 
+import android.util.Log
+import com.example.prayapp.data.PrayData
+import com.example.prayapp.ui.Status
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 object Client {
 
     //make first request
 
+    private val okHttpClient = OkHttpClient()
+    private val urlokhttp = "https://api.pray.zone/v2/times/today.json?"
+    private val gson = Gson()
 
+    fun initRequest(city: String): Status<PrayData> {
+
+        Log.i("mega", "make Request")
+
+        val request = Request.Builder().url("${urlokhttp}city=$city").build()
+        val response = okHttpClient.newCall(request).execute()
+
+        return if (response.isSuccessful) {
+            val parserResponse = gson.fromJson(
+                response.body?.string(),
+                PrayData::class.java
+            )
+            Status.Success(parserResponse)
+        } else {
+            Status.Error(response.message)
+        }
+    }
 
 
 
--- app/src/main/java/com/example/prayapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/prayapp/ui/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.prayapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.prayapp.R
+import com.example.prayapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
