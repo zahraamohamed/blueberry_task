d0a3532f9a8c79f2c211c658e5111f5f0229958a
Karrar Jabbar <alioop137@gmail.com>
2021-09-25 22:42:01 +0300

    feat: done design

--- app/src/main/java/com/example/prayapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/prayapp/ui/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.prayapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.prayapp.R
+import com.example.prayapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomSheet()
+    }
+
+    private fun initBottomSheet() {
+        BottomSheetBehavior.from(binding.bottomSheetPrayTime).apply {
+            peekHeight = 200
+            state = BottomSheetBehavior.STATE_COLLAPSED
+        }
     }
 }
